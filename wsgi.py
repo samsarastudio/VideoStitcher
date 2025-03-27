640f7307565ffa9be25e9f8805ca3739c1f729b1
Nandu <nandu@tuna.group>
2025-03-27 04:54:10 -0400

    Build v1.0.24

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the project root directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from api.app import app
 
 if __name__ == "__main__":
